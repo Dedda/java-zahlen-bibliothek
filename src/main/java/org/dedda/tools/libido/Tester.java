d5a70818dd4686e8cd486d1a0dbd82d89ea58897
Stephan Göppentin <dedda102@gmail.com>
2015-07-31 18:07:26 +0200

    first attempts

---
+++ src/main/java/org/dedda/tools/libido/Tester.java
@@ -0,0 +1,12 @@
+package org.dedda.tools.libido;
+
+/**
+ * Created by dedda on 7/31/15.
+ */
+public class Tester {
+
+    public static void main(String[] args) {
+        System.out.println(Zahlen.Eins);
+    }
+
+}
---
+++ src/main/java/org/dedda/tools/libido/Zahlen.java
@@ -0,0 +1,20 @@
+package org.dedda.tools.libido;
+
+import java.util.Map;
+
+import static java.lang.Math.E;
+import static java.lang.Math.PI;
+import static java.lang.Math.floor;
+
+/**
+ * Created by dedda on 7/31/15.
+ *
+ * @author dedda
+ */
+public class Zahlen {
+
+    public static final int Eins = ((int)PI)%((int)E);
+    public static final int Zwei = (int)floor(E);
+    public static final int Drei = (int)floor(PI);
+
+}
