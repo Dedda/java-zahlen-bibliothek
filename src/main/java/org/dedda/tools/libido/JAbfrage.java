353a1488804ebf5dc66b94df0564de8f98d65d31
Stephan Goeppentin <dedda102@gmail.com>
2015-09-09 17:05:37 +0200

    leere JAbfrage-klasse hinzugefügt

---
+++ src/main/java/org/dedda/tools/libido/JAbfrage.java
@@ -0,0 +1,16 @@
+package org.dedda.tools.libido;
+
+/**
+ * Created by dedda on 9/9/15.
+ *
+ * @author dedda
+ */
+public final class JAbfrage {
+
+    public static Object $(final String $abfrage) {
+        return null;
+    };
+
+    private JAbfrage() {}
+
+}
