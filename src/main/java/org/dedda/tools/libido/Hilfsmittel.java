55d4d315b32ec1a8cdc75cb1e753621445f5f915
Stephan Goeppentin <dedda102@gmail.com>
2015-09-09 17:37:40 +0200

    ein paar hilfsmittel, die aus php bekannt sind hinzugefügt

---
+++ src/main/java/org/dedda/tools/libido/Hilfsmittel.java
@@ -0,0 +1,24 @@
+package org.dedda.tools.libido;
+
+/**
+ * Created by dedda on 9/9/15.
+ *
+ * @author dedda
+ */
+public final class Hilfsmittel {
+
+    public static Object[] feld(Object... $objekte) {
+        return $objekte;
+    }
+
+    public static int zähle(final Object[] $feld) {
+        return $feld.length;
+    }
+
+    public static boolean leer(final Object[] $feld) {
+        return $feld.length == 0;
+    }
+
+    private Hilfsmittel() {}
+
+}
