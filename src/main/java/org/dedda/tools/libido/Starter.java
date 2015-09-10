490ad6604850d36bc332a41ccdc2191ceb63d76f
Stephan Goeppentin <dedda102@gmail.com>
2015-09-10 10:07:22 +0200

    Fehlerhandlung hinzugefügt

--- src/main/java/org/dedda/tools/libido/Starter.java
+++ src/main/java/org/dedda/tools/libido/Starter.java
@@ -1,7 +1,9 @@
 package org.dedda.tools.libido;
 
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 
 import static org.dedda.tools.libido.$.$_ENV;
@@ -15,18 +17,53 @@ import static org.dedda.tools.libido.$.$argv;
  */
 public class Starter {
 
-    public static void main(String[] $args) throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
-        $argv = $args;
-        $argc = $args.length;
-        $_ENV = new HashMap<>();
-        $_ENV.put("Entkäferer", "ausgeschaltet");
-        for (String $arg : $args) {
-            if ($arg.equals("--entwanzen")) {
-                $_ENV.put("Entkäferer", "eingeschaltet");
+    public static void main(String[] $args) {
+        try {
+            $argv = $args;
+            $argc = $args.length;
+            $_ENV = new HashMap<>();
+            $_ENV.put("Entkäferer", "ausgeschaltet");
+            for (String $arg : $args) {
+                if ($arg.equals("--entwanzen")) {
+                    $_ENV.put("Entkäferer", "eingeschaltet");
+                }
             }
+        } catch (Exception $e) {
+            System.out.println("Das war wohl ein interner Fehler!");
         }
-        Class $programmKlasse = Class.forName("standardPaket.Programm");
-        $programmKlasse.getMethod("start").invoke($programmKlasse.getConstructor().newInstance());
+        Class $programmKlasse = null;
+        try {
+            $programmKlasse = Class.forName("standardPaket.Programm");
+        } catch (ClassNotFoundException $e) {
+            System.out.println("Du hast wohl keine \'standardPaket.Programm\'-Klasse erstellt!");
+        }
+        Method $startMethode = null;
+        try {
+            $startMethode = $programmKlasse.getMethod("start");
+        } catch (NoSuchMethodException $e) {
+            System.out.println("In deiner \'standardPaket.Programm\'-Klasse gibt es wohl keine \'start()\'-Methode!");
+        }
+        Constructor $erschaffer = null;
+        try {
+             $erschaffer = $programmKlasse.getConstructor();
+        } catch (NoSuchMethodException $e) {
+            System.out.println("Da scheint was mit deinem Konstruktor nicht zu stimmen!");
+        }
+        Object $instanz = null;
+        try {
+            $instanz = $erschaffer.newInstance();
+        } catch (Exception $e) {
+            System.out.println("Fehler beim Instanziieren!");
+        }
+        try {
+            $startMethode.invoke($instanz);
+        } catch (IllegalAccessException $e) {
+            System.out.println("Deine \'start()\'-Methode konnte nicht aufgerufen werden!");
+        } catch (InvocationTargetException $e) {
+            System.out.println("Deine \'start()\'-Methode konnte nicht aufgerufen werden!");
+        } catch (Exception $e) {
+            System.out.println("Du scheinst kein guter Programmierer zu sein. Ich habe einen Fehler festgestellt!");
+        }
     }
 
 }
