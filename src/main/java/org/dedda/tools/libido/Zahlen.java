ca8a80a10bd09fb325caac0aaae2ee2af009a20b
Stephan Göppentin <dedda102@gmail.com>
2015-09-14 22:18:08 +0200

    zahlen werden aus einer json-datei geladen

--- src/main/java/org/dedda/tools/libido/Zahlen.java
+++ src/main/java/org/dedda/tools/libido/Zahlen.java
@@ -1,16 +1,13 @@
 package org.dedda.tools.libido;
 
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
-import static java.lang.Math.E;
-import static java.lang.Math.PI;
-import static java.lang.Math.floor;
-import static java.lang.Math.log;
-import static java.lang.Math.pow;
-import static java.lang.Math.sin;
-import static java.lang.Math.toDegrees;
-import static java.lang.Math.toRadians;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import static org.dedda.tools.libido.$.__z;
+import static org.dedda.tools.libido.$.$;
 
 /**
  * Created by dedda on 7/31/15.
@@ -19,104 +16,116 @@ import static java.lang.Math.toRadians;
  */
 public final class Zahlen {
 
-    public static final long Eins() {
-        return (long) PI % (long) E;
+    public static final Number Eins() {
+        return __z($(____("1")));
     }
-    public static final long Ers() {
+    public static final Number Ers() {
         return Eins();
     }
-    public static final long Zwei() {
-        return (long) floor(E);
+    public static final Number Zwei() {
+        return __z($(____("2")));
     }
-    public static final long Drei() {
-        return (long) floor(PI);
+    public static final Number Drei() {
+        return __z($(____("3")));
     }
-    public static final long Drit() {
+    public static final Number Drit() {
         return Drei();
     }
-    public static final long Vier() {
-        return (long) pow(floor(E), floor(E));
+    public static final Number Vier() {
+        return __z($(____("4")));
     }
-    public static final long Fünf() {
-        return (long) (toDegrees(sin(E)) / PI - floor(E));
+    public static final Number Fünf() {
+        return __z($(____("5")));
     }
-    public static final long Sechs() {
-        return (((byte) floor(PI)) << (byte) (floor(PI) % floor(E)));
+    public static final Number Sechs() {
+        return __z($(____("6")));
     }
-    public static final long Sieben() {
-        return Arrays.stream(new long[]{(long) E, (long) PI}).map(l -> l == (long) E ? l * (long) E : l).sum();
+    public static final Number Sieben() {
+        return __z($(____("7")));
     }
-    public static final long Sieb() {
+    public static final Number Sieb() {
         return Sieben();
     }
-    public static final long Acht() {
-        return Long.parseLong(((long) (toDegrees(PI * E)) + "").charAt((int) (floor(PI) - floor(E))) + "");
+    public static final Number Acht() {
+        return __z($(____("8")));
     }
-    public static final long Neun() {
-        return (long) (pow((long) PI, (long) PI) / floor(PI));
+    public static final Number Neun() {
+        return __z($(____("9")));
     }
-    public static final long Zehn() {
-        return 10;
+    public static final Number Zehn() {
+        return __z($(____("10")));
     }
-    public static final long Elf() {
-        return 11;
+    public static final Number Elf() {
+        return __z($(____("11")));
     }
-    public static final long Zwölf() {
-        return 12;
+    public static final Number Zwölf() {
+        return __z($(____("12")));
     }
-    public static final long Dreizehn() {
-        return 13;
+    public static final Number Dreizehn() {
+        return __z($(____("13")));
     }
-    public static final long Vierzehn() {
-        return 14;
+    public static final Number Vierzehn() {
+        return __z($(____("14")));
     }
-    public static final long Fünfzehn() {
-        return 15;
+    public static final Number Fünfzehn() {
+        return __z($(____("15")));
     }
-    public static final long Sechzehn() {
-        return 16;
+    public static final Number Sechzehn() {
+        return __z($(____("16")));
     }
-    public static final long Siebzehn() {
-        return 17;
+    public static final Number Siebzehn() {
+        return __z($(____("17")));
     }
-    public static final long Achtzehn() {
-        return 18;
+    public static final Number Achtzehn() {
+        return __z($(____("18")));
     }
-    public static final long Neunzehn() {
-        return 19;
+    public static final Number Neunzehn() {
+        return __z($(____("19")));
     }
-    public static final long Zwanzig() {
-        return 20;
+    public static final Number Zwanzig() {
+        return __z($(____("20")));
     }
-    public static final long Einundzwanzig() {
-        return 21;
+    public static final Number Einundzwanzig() {
+        return __z($(____("21")));
     }
-    public static final long Zweiungzwanzig() {
-        return 22;
+    public static final Number Zweiungzwanzig() {
+        return __z($(____("22")));
     }
-    public static final long Dreiundzwanzig() {
-        return 23;
+    public static final Number Dreiundzwanzig() {
+        return __z($(____("23")));
     }
-    public static final long Vierundzwanzig() {
-        return 24;
+    public static final Number Vierundzwanzig() {
+        return __z($(____("24")));
     }
-    public static final long Fünfundzwanzig() {
-        return 25;
+    public static final Number Fünfundzwanzig() {
+        return __z($(____("25")));
     }
-    public static final long Sechsundzwanzig() {
-        return 26;
+    public static final Number Sechsundzwanzig() {
+        return __z($(____("26")));
     }
-    public static final long Siebenundzwanzig() {
-        return 27;
+    public static final Number Siebenundzwanzig() {
+        return __z($(____("27")));
     }
-    public static final long Achtundzwanzig() {
-        return 28;
+    public static final Number Achtundzwanzig() {
+        return __z($(____("28")));
     }
-    public static final long Neunundzwanzig() {
-        return 29;
+    public static final Number Neunundzwanzig() {
+        return __z($(____("29")));
     }
-    public static final long Dreißig() {
-        return 30;
+    public static final Number Dreißig() {
+        return __z($(____("30")));
+    }
+
+    public static String ____(final String $zahlAlsText) {
+        try {
+            JsonReader $leser = Json.createReader(new FileInputStream(new File("zahlen.json")));
+            JsonObject $wurzel = $leser.readObject();
+            String $zahl = $wurzel.getString($zahlAlsText);
+            return $zahl + ";";
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return "0;";
     }
 
     private Zahlen() {}
---
+++ src/test/java/org/dedda/tools/libido/ZahlenTest.java
@@ -0,0 +1,43 @@
+package org.dedda.tools.libido;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.dedda.tools.libido.$.$;
+import static org.dedda.tools.libido.$.__kz;
+import static org.dedda.tools.libido.Zahlen.____;
+import static org.junit.Assert.*;
+
+/**
+ * Created by dedda on 9/14/15.
+ *
+ * @author dedda
+ */
+@RunWith(Parameterized.class)
+public class ZahlenTest {
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        List<Object[]> $objektListe = new ArrayList<>();
+        for (int i = 1; i <= 30; i++) {
+            $objektListe.add(new Object[]{i});
+        }
+        return $objektListe;
+    }
+
+    @Parameter
+    public int $zahl;
+
+    @Test
+    public void testZahlen() throws Exception {
+        assertEquals((double) $zahl, __kz($(____("" + $zahl))), 0.01f);
+    }
+
+}
