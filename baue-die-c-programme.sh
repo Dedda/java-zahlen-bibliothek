07ce6c820471ec82f3b324fbe9f11052588e0aed
Stephan Göppentin <dedda102@gmail.com>
2015-11-09 20:19:08 +0100

    rasante fibonaccifolge reingeklatscht!

--- baue-die-c-programme.sh
+++ baue-die-c-programme.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo 'Baue Hochgeschwindigkeitsrechnungen...'
-gcc -m32 -o hochgeschwindigkeitsrechnungen ./src/main/c/hochgeschwindigkeitsrechnungen.c ./src/main/asm/hochgeschwindigkeitsaddition.S ./src/main/asm/hochgeschwindigkeitssubtraktion.S ./src/main/asm/hochgeschwindigkeitsmultiplikation.S ./src/main/asm/hochgeschwindigkeitsdivision.S
+gcc -m32 -o hochgeschwindigkeitsrechnungen ./src/main/c/hochgeschwindigkeitsrechnungen.c ./src/main/asm/hochgeschwindigkeitsaddition.S ./src/main/asm/hochgeschwindigkeitssubtraktion.S ./src/main/asm/hochgeschwindigkeitsmultiplikation.S ./src/main/asm/hochgeschwindigkeitsdivision.S ./src/main/asm/fibonacci.S
 echo 'OK'
--- src/main/c/hochgeschwindigkeitsrechnungen.c
+++ src/main/c/hochgeschwindigkeitsrechnungen.c
@@ -5,12 +5,15 @@ extern int hochgeschwindigkeitsaddition(int a, int b);
 extern int hochgeschwindigkeitssubtraktion(int a, int b);
 extern int hochgeschwindigkeitsmultiplikation(int a, int b);
 extern int hochgeschwindigkeitsdivision(int a, int b);
+extern int fibonacci(int n);
 
 int main(int argc, char *argv[]) {
     int a;
     sscanf(argv[2], "%d", &a);
-    int b;
-    sscanf(argv[3], "%d", &b);
+    int b = 0;
+    if (argc > 3) {
+        sscanf(argv[3], "%d", &b);
+    }
     int c = 0;
     if (strcmp(argv[1], "addieren") == 0) {
         c = hochgeschwindigkeitsaddition(a, b);
@@ -20,6 +23,8 @@ int main(int argc, char *argv[]) {
         c = hochgeschwindigkeitsmultiplikation(a, b);
     } else if (strcmp(argv[1], "dividieren") == 0) {
         c = hochgeschwindigkeitsdivision(a, b);
+    } else if (strcmp(argv[1], "fibonacci") == 0) {
+        c = fibonacci(a);
     }
     printf("%d\n", c);
 }
